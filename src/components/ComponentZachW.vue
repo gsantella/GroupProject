81b887da8249be863a569b45cbadf5164a396e48
Willettza62 <148791350+Willettza62@users.noreply.github.com>
2023-11-08 16:49:36 +0000

    i did my part 2 electric boogaloo

--- src/components/ComponentZachW.vue
+++ src/components/ComponentZachW.vue
@@ -1,2 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Zach W</h1>
+  <h2><marquee>Made you look</marquee></h2>
+</template>
+
+<style scoped>
+
+</style>
 <h1>Zach W</h1>
 <h2><marquee>Made you look</marquee></h2>
