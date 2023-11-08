121a860413549e503753fd08e8c7ff663c867213
gordondavis8379 <148791347+gordondavis8379@users.noreply.github.com>
2023-11-08 16:49:30 +0000

    fixed

--- src/components/ComponentGordon.vue
+++ src/components/ComponentGordon.vue
@@ -1,7 +1,16 @@
-<h1><marquee>Gordon</marquee></h1>
+<script setup>
+
+</script>
+
+<template>
+  <h1><marquee>Gordon</marquee></h1>
+</template>
 
 <style scoped>
   h1 {
     color: mediumpurple;
   }
 </style>
+
+
+
