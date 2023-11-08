773d83649240da1752c20e1f27ef308e0a04ea93
Oppslia <148699849+Oppslia@users.noreply.github.com>
2023-11-08 16:58:31 +0000

    fk the engineer

---
+++ src/components/ComponentShawn.vue
@@ -0,0 +1,16 @@
+<script setup>
+function guidoButton(){
+  document.getElementById("add_message").innerHTML = "<marquee>Guido<marquee/>"
+}
+  
+</script>
+
+<template>
+  <button id ="add_message" @click="guidoButton()">Add Message</button>
+  
+</template>
+
+<style scoped>
+
+  
+</style>
