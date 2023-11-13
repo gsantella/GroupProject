e777fb4c8b11e7d4afd9c136968580c777d3cce8
gordondavis8379 <148791347+gordondavis8379@users.noreply.github.com>
2023-11-13 17:00:00 +0000

    fixed button

--- src/components/ComponentGordon.vue
+++ src/components/ComponentGordon.vue
@@ -1,13 +1,13 @@
 <script setup>
-  const showMessage = () => {
-    alert('wow');
-  };
+  function clickMe() { 
+    alert('I can not believe that you clicked the button. I am pretty sure that it said do not click me let me check. Yup it says do not click me and you clicked it anyways. Why? Why did you click the button? Do you always ignore directions?');
+ }
 </script>
 
 
 <template>
   <h1><marquee>Gordon</marquee></h1>
-  <button>Don't you dare click me</button>
+  <button @click="clickMe()">Don't you dare click me</button>
 </template>
 
 <style scoped>
