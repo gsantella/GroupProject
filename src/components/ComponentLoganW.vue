f3b6a912f8b7f53c36495e345a662537fa153168
AllenWilt <75284469+AllenWilt@users.noreply.github.com>
2023-11-13 16:49:46 +0000

    done

---
+++ src/components/ComponentLoganW.vue
@@ -0,0 +1,18 @@
+<scipt setup>
+
+</scipt>
+
+<template>
+
+<marquee>I'm Hungry for Pizza! ~Logan Wilt</marquee>
+<marquee>Hello there!</marquee>
+
+</template>
+
+<style scoped>
+  marquee {
+    color: red;
+    font: 12px Arial;
+    Border: 16px dotted green;
+  }
+</style>
