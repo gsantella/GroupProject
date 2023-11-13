c5024cef4b4577583e11796c28bb3606dafac42b
gordondavis8379 <148791347+gordondavis8379@users.noreply.github.com>
2023-11-13 16:39:57 +0000

    added button

--- src/components/ComponentGordon.vue
+++ src/components/ComponentGordon.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+  const showMessage = () => {
+    alert('wow');
+  };
 </script>
+
 
 <template>
   <h1><marquee>Gordon</marquee></h1>
+  <button>Don't you dare click me</button>
 </template>
 
 <style scoped>
