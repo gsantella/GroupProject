10aca98299da8bc896fbfdff3d68466a2723db8a
UndisputedFN9 <83854834+UndisputedFN9@users.noreply.github.com>
2023-11-14 21:31:07 +0000

    Made a button and marquee and updated colors on H1 on ComponentDevin

--- src/components/ComponentDevin.vue
+++ src/components/ComponentDevin.vue
@@ -1,13 +1,22 @@
 <script setup>
-  function devin() {
-    alert("We love coding")
+  function devin() { 
+      alert("You fell into my trap")
+      alert("Just three clicks away")
+      alert("Only two to go!!!")
+      alert("ONE MORE")
+      alert("Sorry, i lied. Its the next one")
+      alert("CRAZY! I WAS CRAZY ONCE. THEY LOCKED ME IN A ROOM, A RUBBER ROOM, A RUBBER ROOM WITH RATS, AND RATS MAKE ME CRAZY.") 
   }
 </script>
 
 <template>
-  <h1>Devin</h1>  
+  <h1>Devin</h1>
+  <button @click="devin">Click me if you're crazy</button>
+  <marquee>CLICK ME IF YOU'RE CRAZY</marquee>
 </template>
 
 <style scoped>
-  color: red
+  h1 { background-color: crimson; }
+  button { background-color: crimson; }
+  marquee { background-color: crimson; }
 </style>
