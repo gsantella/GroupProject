731c8e9c85f4068260bd86037cf72100c1f3426d
Willettza62 <148791350+Willettza62@users.noreply.github.com>
2023-11-14 21:23:19 +0000

    i made yellow green red button
    crayons are edible if you try hard enough

--- src/components/ComponentZachW.vue
+++ src/components/ComponentZachW.vue
@@ -1,12 +1,25 @@
 <script setup>
-
+  import { ref } from 'vue'
+  const count = ref(0)
+  function clickMe() { 
+      alert('you just took orders from a button, didnt know you stooped so low');
+   }
 </script>
 
-<template>
-  <h1>Zach W</h1>
-  <h2><marquee>Made you look</marquee></h2>
-</template>
-
-<style scoped>
-
-</style>
+ <template>
+  <h1>Zach Willett</h1>
+  <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+    <button @click="clickMe()">press this button</button>
+   ///
+<button @click="count++"><h1>You clicked me {{ count }} times.</h1></button>
+   ///
+     <strong>satisfying right?</strong>
+<p>
+   <button style="background-color: red;">Stop</button>
+   <button style="background-color: Yellow;">SPEED UP</button>
+   <button style="background-color: green;">Go</button>
+</p>
+   <div>
+     
+   </div>
+</template> 
