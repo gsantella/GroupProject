f3f183a229ce7732bfb0abdb568b8cdfafe29d8c
UndisputedFN9 <83854834+UndisputedFN9@users.noreply.github.com>
2023-11-14 20:55:10 +0000

    Changed color of text in ComponentDevin

--- src/components/ComponentDevin.vue
+++ src/components/ComponentDevin.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+  function devin() {
+    alert("We love coding")
+  }
 </script>
 
 <template>
+  <div class="devin">
   <h1>Devin</h1>  
 </template>
 
 <style scoped>
-
+  color: red
 </style>
