b0d17cbc66f11f1e95b3ef615a1d5426dbbb8ef4
danielhirschbiel <148791467+danielhirschbiel@users.noreply.github.com>
2023-11-14 20:57:11 +0000

    Added Component

---
+++ src/components/ComponentAsh.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="i in 20" :key="i">{{ i }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: Array.from({ length: 20 }, (v, k) => k + 1)
+    }
+  }
+}
+</script>
