fefbbce8902c5e87f29bcbea962c990be291f93d
LGates99 <148791409+LGates99@users.noreply.github.com>
2023-11-14 21:28:51 +0000

    new button

--- src/components/ComponentLoganG.vue
+++ src/components/ComponentLoganG.vue
@@ -3,12 +3,15 @@
 </script>
 
 <template>
-  <button onclick="alert('Why did you click me.')" class="button is-info is-rounded" >Logan</button>
+  <hi><button onclick="alert('Why did you click me.')" class="button is-info is-rounded" >Logan</button></hi>
   <progress class="progress is-medium is-dark" max="100">45%</progress>
+  <button onclick="alert('.-- .... -.-- / -.. .. -.. / -.-- --- ..- / -.-. .-.. .. -.-. -.- / -- . / --..-- / -.. --- / -.-- --- ..- / . ...- . -. / ..- -. -.. . .-. ... - .- -. -.. / .-- .... .- - / .. / .- -- / ... .- -.-- .. -. --. / .-- . .-.. .-.. / .. / --. ..- . ... ... / .. - / .. - / - .. -- . / - --- / -.-. .... .- -. --. . / .-.. .- -. --. ..- .- --. ./ ∴⍑|| ᔑ∷ᒷ ||𝙹⚍ ℸ ̣ ∷ᔑリᓭꖎᔑℸ ̣ ╎リ⊣ ᒲᒷ, ᔑᒲ i ℸ ̣ ⍑ᔑℸ ̣  ╎リℸ ̣ ᒷ∷ᒷᓭℸ ̣ ╎リ⊣. ')">
+    -.-. .-.. .. -.-. -.- / -- .
+  </button>
 </template>
 
 <style scoped>
-h1 {
-  color: #green;
+button {
+  text-decoration-line: false;
 }
 </style>
