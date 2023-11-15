77b3f28a21d6f6274828a1c9088f278716bea99e
RGHoppa <114625689+RGHoppa@users.noreply.github.com>
2023-11-15 13:53:01 +0000

    Added a button to display the date and time

--- src/components/ComponentSkyler.vue
+++ src/components/ComponentSkyler.vue
@@ -6,6 +6,11 @@
   <body>
 
 <h1 style="color:blue;">Skyler Steele</h1>
+    <br>
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Click me to display Date and Time.</button>
+    <p id="demo"></p>
   </body>
 </template>
 
