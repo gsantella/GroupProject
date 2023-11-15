0b2aacab418ea7186553d5a581f8e0a3c86c7f49
LGates99 <148791409+LGates99@users.noreply.github.com>
2023-11-15 18:20:01 +0000

    change function

--- src/components/ComponentLoganG.vue
+++ src/components/ComponentLoganG.vue
@@ -1,15 +1,43 @@
  <script setup>
   function crazy(){
      alert(':)')
-    while (true) { alert('I mm not crazy your crazy, ╎ ᓵ⚍∷ᓭᒷ ||𝙹⚍ ℸ ̣ 𝙹 𝙹リꖎ|| ⊣ᒷℸ ̣  ʖᔑリᒷ 𝙹⎓ ᔑ∷ℸ ̣ ⍑∷𝙹!¡𝙹↸ᓭ 𝙹リ ||𝙹⚍∷ ↸╎ᔑᒲ𝙹リ↸ ᓭ∴𝙹∷↸ ∴⍑ᒷリ ||𝙹⚍ ↸𝙹 ᔑ ꖎᒷ⍊ᒷꖎ 30 ᒷリᓵ⍑ᔑリℸ ̣ ᒲᒷリℸ ̣  ⎓𝙹∷ ℸ ̣ ⍑ᒷ ∷ᒷᓭℸ ̣  𝙹⎓ ||𝙹⚍∷ ꖎ╎⎓ᒷ.')
+     alert('I mm not crazy your crazy, ╎ ᓵ⚍∷ᓭᒷ ||𝙹⚍ ℸ ̣ 𝙹 𝙹リꖎ|| ⊣ᒷℸ ̣  ʖᔑリᒷ 𝙹⎓ ᔑ∷ℸ ̣ ⍑∷𝙹!¡𝙹↸ᓭ 𝙹リ ||𝙹⚍∷ ↸╎ᔑᒲ𝙹リ↸ ᓭ∴𝙹∷↸ ∴⍑ᒷリ ||𝙹⚍ ↸𝙹 ᔑ ꖎᒷ⍊ᒷꖎ 30 ᒷリᓵ⍑ᔑリℸ ̣ ᒲᒷリℸ ̣  ⎓𝙹∷ ℸ ̣ ⍑ᒷ ∷ᒷᓭℸ ̣  𝙹⎓ ||𝙹⚍∷ ꖎ╎⎓ᒷ.')
 
-  }
 }
 </script>
 
 <template>
  <hi><button onclick="alert('Why did you click me.')" class="button is-info is-rounded" >Logan</button></hi>
   <progress class="progress is-medium is-dark" max="100">45%</progress>
+
+  
+  <div class="file">
+    <label class="file-label">
+      <input class="file-input" type="file" name="resume">
+      <span class="file-cta">
+        <span class="file-icon">
+          <i class="fas fa-upload"></i>
+        </span>
+        <span class="file-label">
+          Choose a file…
+        </span>
+      </span>
+    </label>
+  </div>
+
+  
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="answer">
+      Yes
+    </label>
+    <label class="radio">
+      <input type="radio" name="answer">
+      No
+    </label>
+  </div>
+
+  
   <button onclick="alert('.-- .... -.-- / -.. .. -.. / -.-- --- ..- / -.-. .-.. .. -.-. -.- / -- . / --..-- / -.. --- / -.-- --- ..- / . ...- . -. / ..- -. -.. . .-. ... - .- -. -.. / .-- .... .- - / .. / .- -- / ... .- -.-- .. -. --. / .-- . .-.. .-.. / .. / --. ..- . ... ... / .. - / .. - / - .. -- . / - --- / -.-. .... .- -. --. . / .-.. .- -. --. ..- .- --. ./ ∴⍑|| ᔑ∷ᒷ ||𝙹⚍ ℸ ̣ ∷ᔑリᓭꖎᔑℸ ̣ ╎リ⊣ ᒲᒷ, ᔑᒲ i ℸ ̣ ⍑ᔑℸ ̣  ╎リℸ ̣ ᒷ∷ᒷᓭℸ ̣ ╎リ⊣. ')">
     -.-. .-.. .. -.-. -.- / -- .
   </button>
