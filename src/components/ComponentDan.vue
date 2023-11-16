410b1c08c2356f40e87f0fe6fb3bd72a21f80d53
dstitt0516 <148791450+dstitt0516@users.noreply.github.com>
2023-11-16 21:44:09 +0000

    Test 5

--- src/components/ComponentDan.vue
+++ src/components/ComponentDan.vue
@@ -1,27 +1,16 @@
-<script setup>
-  let count = 0
-  function increment() {
-  document.getElementById("clicker").innerHTML = "You shouldn't have clicked me " + count + " times" ;
-    count--;
-  }
-
+<script>
+  
 </script>
 
+
 <template>
-  <h1><marquee>This is Daniel's button no touch</marquee></h1>
-  <button id="clicker" @click="increment()">Button</button>
 
-
-
-
+ <h1>Dan</h1>
+  
 </template>
 
-<style scoped>
-h1 {
-  color: darkblue;
-}
-button {
-align: center;
-}
-</style>
 
+
+<style scoped>
+
+</style>
