e18efd62d32d20774b87609ccab2a1e7510e87f3
dstitt0516 <148791450+dstitt0516@users.noreply.github.com>
2023-11-16 21:28:23 +0000

    Test1

--- src/components/ComponentDan.vue
+++ src/components/ComponentDan.vue
@@ -1,9 +1,9 @@
 <script>
-  // let count = 0
-  // function increment() {
-  // document.getElementById("clicker").innerHTML = "You shouldn't have clicked me " + count + " times >:o" ;
-  //   count--;
-  // }
+  let count = 0
+  function increment() {
+    document.getElementById("clicker").innerHTML = "You shouldn't have clicked me " + count + " times >:o" ;
+    count--;
+  }
   export default {
     data() {
       return {
@@ -16,37 +16,21 @@
       }
     }
   };
-  
 </script>
-
-
-
-
-
-
-
-
-
-
-
 <template>
-  <!--<h1><marquee>This is Daniel's button no touch</marquee></h1>
-  <button id="clicker" @click="increment()">Not A Button!! >:(</button>-->
-  
-  
-  <button @click="toggleAlignment" :style="{ float: alignLeft ? 'left' : 'none' }">Click me</button>
-  
-  
-   
-
-
+  <div>
+    <h1><marquee>This is Daniel's button no touch</marquee></h1>
+    <button id="clicker" @click="increment()">Not A Button!! >:(</button>
+    <button @click="toggleAlignment" :style="{ float: alignLeft ? 'left' : 'none' }">Click me</button>
+  </div>
 </template>
-
 <style scoped>
 h1 {
   color: darkblue;
 }
 button {
-align: center;
+  align: center;
 }
 </style>
+
+
