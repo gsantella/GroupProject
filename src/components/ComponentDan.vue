809d68f0dd2903508e41c1cb10be67b56dbbc1ba
dstitt0516 <148791450+dstitt0516@users.noreply.github.com>
2023-11-16 21:22:14 +0000

    Wrong one

--- src/components/ComponentDan.vue
+++ src/components/ComponentDan.vue
@@ -1,9 +1,9 @@
 <script>
-  let count = 0
-  function increment() {
-    document.getElementById("clicker").innerHTML = "You shouldn't have clicked me " + count + " times >:o" ;
-    count--;
-  }
+  // let count = 0
+  // function increment() {
+  // document.getElementById("clicker").innerHTML = "You shouldn't have clicked me " + count + " times >:o" ;
+  //   count--;
+  // }
   export default {
     data() {
       return {
@@ -16,17 +16,37 @@
       }
     }
   };
+  
 </script>
+
+
+
+
+
+
+
+
+
+
+
 <template>
-  <h1><marquee>This is Daniel's button no touch</marquee></h1>
-  <button id="clicker" @click="increment()">Not A Button!! >:(</button>
+  <!--<h1><marquee>This is Daniel's button no touch</marquee></h1>
+  <button id="clicker" @click="increment()">Not A Button!! >:(</button>-->
+  
+  
   <button @click="toggleAlignment" :style="{ float: alignLeft ? 'left' : 'none' }">Click me</button>
+  
+  
+   
+
+
 </template>
+
 <style scoped>
 h1 {
   color: darkblue;
 }
 button {
-  align: center;
+align: center;
 }
 </style>
