a3117c13a65cd933911d0824e0a2d0fc3ea4a40a
adamlacue <117120196+adamlacue@users.noreply.github.com>
2023-11-29 14:51:12 +0000

    Added more counter stuff

--- src/components/ComponentAdam.vue
+++ src/components/ComponentAdam.vue
@@ -5,6 +5,8 @@
 
 <template>
   <marquee>Adam</marquee>
+  <h1>Mycounter = {{ count }}</h1>
+  <h1>Mycounter = {{ count * 10}}</h1>
   <p>This is a paragraph</p>
   <button @click="count++">Count is: {{ count }}</button>
 </template>
