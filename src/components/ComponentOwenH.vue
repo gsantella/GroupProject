a2d14ba4e956f20713b9784d332eb0a2f8d9fd3a
SquidO22 <121193544+SquidO22@users.noreply.github.com>
2023-11-29 15:02:18 +0000

    Changed Button functionality

--- src/components/ComponentOwenH.vue
+++ src/components/ComponentOwenH.vue
@@ -6,7 +6,8 @@
 <template>
   <h1>This is my special zone</h1>
   <p>This is a paragraph tag!</p>
-  <button @click="count++">Count is: {{ count }}</button>
+  <h2>My button has been clicked {{ count }} times!</h2>
+  <button @click="count++">BUTTON</button>
 </template>
 
 <style scoped>
