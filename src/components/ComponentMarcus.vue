151aa0050fdaf35a6bedb599e869b8eaf50cb42c
sorucoder <sorucoder+github@proton.me>
2023-11-29 14:45:16 +0000

    Updated my component.
    Added a few wizbang features.

--- src/components/ComponentMarcus.vue
+++ src/components/ComponentMarcus.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+    import {ref} from 'vue';
+    const count = ref(0)
 </script>
 
 <template>
-  <marquee>Marcus</marquee>
+    <marquee>Marcus</marquee>
+    <p>This is a paragraph.</p>
+    <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
