5e1e7afc81a84c4849196c206e8b8d5b89cc969c
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2023-11-29 14:51:03 +0000

    Add some more sounter

--- src/components/ComponentBen.vue
+++ src/components/ComponentBen.vue
@@ -6,6 +6,8 @@
 
 <template>
   <h1>Ben</h1>
+  <h1> My counter = {{ count }}</h1>
+  <h1> My counter = {{ count * 10 / 5 }}</h1>
   <p>This is a paragraph</p>
   <button @click="count++">Count is: {{ count }}</button>
 </template>
