3d1ded93e732c22bc7280031db345dd9da371dad
gsantella <gsantella@southhills.edu>
2023-11-29 14:50:57 +0000

    add some more counter

--- src/components/ComponentGuido.vue
+++ src/components/ComponentGuido.vue
@@ -5,6 +5,8 @@
 
 <template>
   <marquee>Guido</marquee>
+  <h1>My counter = {{ money }}</h1>
+  <h1>My counter * 10 = {{ count * 10 }}</h1>
   <p>This is a paragraph</p>
   <button @click="count++">Count is: {{ count }}</button>
 </template>
