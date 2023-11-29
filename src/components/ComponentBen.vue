c6cb1303eee33b7f15ca915dafee0a5fd349a56e
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2023-11-29 14:45:02 +0000

    made count button and added small paragraph

--- src/components/ComponentBen.vue
+++ src/components/ComponentBen.vue
@@ -1,9 +1,13 @@
-<script setup>
 
+<script setup>
+  import {ref} from 'vue'
+   const count = ref(0)
 </script>
 
 <template>
   <h1>Ben</h1>
+  <p>This is a paragraph</p>
+  <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
