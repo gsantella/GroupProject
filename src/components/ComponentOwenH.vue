51834fb6eb7f9c3a04805aa8ba1bdbe7564ddb3e
SquidO22 <121193544+SquidO22@users.noreply.github.com>
2023-11-29 14:47:42 +0000

    Added button to Owen component

--- src/components/ComponentOwenH.vue
+++ src/components/ComponentOwenH.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+  import {ref} from 'vue'
+  const count = ref(0)
 </script>
 
 <template>
   <h1>This is my special zone</h1>
+  <p>This is a paragraph tag!</p>
+  <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
